46d8ebe51263dd487d8afe0506a07e1b2704de6a
mixail.olesik <mixail.olesik@gmail.com>
2020-03-23 18:18:28 +0300

    Создание интерфейса Rectangle.h

---
+++ Task1/Rectangle.h
@@ -0,0 +1,28 @@
+class Rectangle {
+public:
+	static double num_of_rectangles;
+	static double total_square;
+private:
+	double left_down[2];
+	double right_up[2];
+public:
+	Rectangle();
+	~Rectangle();
+	Rectangle(double *left_down, double *right_up);
+	Rectangle(double* left_down, double height, double width);
+	double get_width();
+	double get_height();
+	double* get_left_down();
+	double* get_left_up();
+	double* get_right_down();
+	double* get_right_up();
+	double get_perimeter();
+	double get_square();
+	double get_r_inner_circle();
+	double get_r_outer_circle();
+	
+	bool is_square();
+	bool check_point(double* coordinats);
+
+	void change_geometry();
+};
