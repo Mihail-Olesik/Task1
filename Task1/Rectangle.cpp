879a7aebdc9959164071db840f5b3151045ed625
mixail.olesik <mixail.olesik@gmail.com>
2020-03-23 19:18:40 +0300

    Методы для геттеров

--- Task1/Rectangle.cpp
+++ Task1/Rectangle.cpp
@@ -1,5 +1,6 @@
 #include "Rectangle.h"
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -45,3 +46,47 @@ double Rectangle::get_num_of_rectangles() {
 double Rectangle::get_total_square() {
 	return total_square;
 }
+
+double Rectangle::get_width() {	//метод, возвращающий ширину прямоугольника
+	return abs(this->left_down[0] - this->right_up[0]);
+}
+
+double Rectangle::get_height() { //метод, возвращающий длину прямоугольника
+	return abs(this->left_down[1] - this->right_up[1]);
+}
+
+double* Rectangle::get_left_down() { //метод, возвращающий координаты левого 
+	return left_down;                //нижнего угла в виде массива
+}
+
+double* Rectangle::get_left_up() { //метод, возвращающий координаты левого верхнего угла в виде массива
+	return new double[2] { this->left_down[0], this->right_up[1] };
+}
+
+double* Rectangle::get_right_down() { //метод, возвращающий координаты правого нижнего угла в виде массива
+	return new double[2]{ this->right_up[0], this->left_down[1] };
+}
+
+double* Rectangle::get_right_up() { //метод, возвращающий координаты правого верхнего угла в виде массива
+	return right_up;
+}
+
+double Rectangle::get_perimeter() { //метод, возвращающий периметр прямоугольника
+	return 2 * this->get_height() + 2 * this->get_width();
+}
+
+double Rectangle::get_square() { //метод, возвращающий площадь прямоугольника
+	return this->get_height() * this->get_height();
+}
+
+double Rectangle::get_r_inner_circle() { //радиус вписанной окружности
+	if (this->is_square())	//только при условии, что прямоугольник -- квадрат можно вписать
+		return this->get_height / 2;
+	else
+		return -1;	//при несоответствии условия возвращается -1
+}
+
+double Rectangle::get_r_outer_circle() { //радиус описанной окружности
+	return sqrt(pow(this->get_height, 2) + pow(this->get_width, 2)) / 2;
+}
+
